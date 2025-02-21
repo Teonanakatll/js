b26c99522b99f4c9ee5c26c51e6c507548ecd1bd
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2025-02-21 20:17:38 +0300

    commit 5 Routing, NavLink

--- 01-first-project/react-kabzda-1/src/App.jsx
+++ 01-first-project/react-kabzda-1/src/App.jsx
@@ -1,15 +1,33 @@
-import Header from "./components/Header";
-import Navbar from "./components/Nav";
-import Profile from "./components/Content";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
+
+// npm react-router-dom
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className={`app-wrapper-content gitem`}>
+          <Routes>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/dialogs/*" element={<Dialogs />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/music" element={<Music />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </div>
+
+      </div>
+    </BrowserRouter>
   );
 };
 
